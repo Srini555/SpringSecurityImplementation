db45c76a7bb445cd677f864d0b2833144597f581
amitaucs <the.amisoft@gmail.com>
2019-08-15 10:06:02 +0530

    In memory

--- guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
@@ -1,36 +1,22 @@
 package com.frankmoley.security.app;
 
-import com.frankmoley.security.app.auth.LandonUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private LandonUserDetailsService userDetailsService;
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
-        return provider;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       auth.authenticationProvider(authenticationProvider());
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -38,11 +24,19 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         http.
                 csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/","/index","/css/*","/js/*","/*console/*").permitAll()
+                .antMatchers("/","/index","/css/*","/js/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }
 
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
 
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder().username("john").password("password").roles("USER","ADMIN").build());
+        users.add(User.withDefaultPasswordEncoder().username("foo").password("foobar").roles("USER").build());
+        return new InMemoryUserDetailsManager(users);
+    }
 }
