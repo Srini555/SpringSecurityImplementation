f17bf807e9e743ed5718975f9a0258de222edb87
amitaucs <the.amisoft@gmail.com>
2019-08-15 09:56:11 +0530

    In memory

--- guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
@@ -38,7 +38,7 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         http.
                 csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/","/index","/css/*","/js/*").permitAll()
+                .antMatchers("/","/index","/css/*","/js/*","/*console/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
